1db02ca1f41f14be93c775007c02cf84bb431f94
NaderHammami <131385829+NaderHammami@users.noreply.github.com>
2023-06-27 00:22:43 +0100

    Add files via upload

---
+++ gliderr.js
@@ -0,0 +1,10 @@
+new Glider(document.querySelector('.gliderr'), {
+    slidesToScroll: 1,
+    slidesToShow: 5.5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prevv',
+      next: '.glider-nextt'
+    }
+  });
